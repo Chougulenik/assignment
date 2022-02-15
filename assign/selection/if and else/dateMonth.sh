92e5be0f1e457630050ac0ee3612427d5b470cda
Chougulenik <nikitachougule47@gmail.com>
2022-02-14 21:54:54 -0500

    remove folder

--- assign/selection/if and else/dateMonth.sh
+++
@@ -1,9 +0,0 @@
-read -p "Enter date : " date
-read -p "Enter Month : " Month
-
-if (($Month <=6 && $date <=20)) && (($Month >=3 && $Month <= 6)) && (($date < 31))
-then
-	echo $Month $date "true"
-else
-	echo "false"
-fi
