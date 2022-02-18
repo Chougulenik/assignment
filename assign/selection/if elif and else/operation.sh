b9c38f4c9c4808d6635097b90a8dab5ae40955d9
Chougulenik <nikitachougule47@gmail.com>
2022-02-17 19:55:53 -0500

    day 5

--- assign/selection/if elif and else/operation.sh
+++
@@ -1,8 +0,0 @@
-echo "enter 3 number"
-read -p "Enter 1 number : " a 
-read -p "Enter 2 number : " b
-read -p "Enter 3 number : " c
-echo $(($a + $b *$c))
-echo $(($a % $b + $c))
-echo $(($c + $a / $b))
-echo $(($a * $b +$c))
